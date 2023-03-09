75681d93ec307e97fd3d5e5eb5e3fd165d89d031
Mohammad Hossein Heydarchi <mhheidarchi2003@yahoo.com>
2023-03-09 10:37:57 +0100

    - Update the GUI layout - Add a TODO file for GUI

--- GUI/main.py
+++ GUI/main.py
@@ -1,8 +1,9 @@
-from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel, QLineEdit, QVBoxLayout, QHBoxLayout, QWidget,QPushButton
 
+from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel, QLineEdit, QFileDialog
+from PyQt5.QtWidgets import QVBoxLayout, QHBoxLayout, QWidget,QPushButton, QListWidget
+from PyQt5.QtCore import Qt
 
 import sys
-
 
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -11,50 +12,81 @@ class MainWindow(QMainWindow):
         self.setWindowTitle("SELinux-Explorer")
 
         self.lblPath = QLabel("Path")
-        self.label2 = QLabel("List of paths")
+        self.lblSelectedPath = QLabel("Selected Path")
+        self.edtCurrentSelectedPath = QLineEdit()
 
-        self.lblSelectedPath = QLabel()
-        self.btnBrowse = QPushButton("Browse")
-        self.btnAddToList = QPushButton("Add to list")
+        self.lstSelectedPath = QListWidget()
+
+        self.btnBrowseFile = QPushButton("Browse File")
+        self.btnBrowseFolder = QPushButton("Browse Folder")
+        self.btnAddToList = QPushButton("Add to")
+        self.btnRemoveFromList = QPushButton("Remove from")
+        self.btnAnalyzeAll = QPushButton("Analyze All")
+        self.btnAnalyzeSelected = QPushButton("Analyze Selected")
+        self.btnClearAnalyze = QPushButton("Clear Analyze")
+
 
         self.layoutPath = QHBoxLayout()
-
-
-        self.layout = QHBoxLayout()
-
+        self.layoutSelectedPath = QHBoxLayout()
+        self.layoutSelectedPathButton = QVBoxLayout()
+        self.layoutAnalyzer = QHBoxLayout()
         self.mainLayout = QVBoxLayout()
 
-
-
         self.container = QWidget()
-
-        # Set the central widget of the Window.
-        self.setCentralWidget(self.container)
 
         self.initUI()
 
     def initUI(self):
+        self.setMinimumWidth(400)
 
-        self.lblSelectedPath.setFixedWidth(280)
-        self.lblSelectedPath.setWordWrap(True)
-        self.btnBrowse.clicked.connect(self.browsePath)
+        #layoutPath
+        self.edtCurrentSelectedPath.setFixedWidth(500)
+        self.edtCurrentSelectedPath.setReadOnly(True)
+
+        self.layoutPath.addWidget(self.lblPath)
+        self.layoutPath.addWidget(self.edtCurrentSelectedPath)
+        self.layoutPath.addWidget(self.btnBrowseFile)
+        self.layoutPath.addWidget(self.btnBrowseFolder)
+        self.layoutPath.addStretch()
+
+        self.btnBrowseFile.clicked.connect(self.browseFilePath)
+        self.btnBrowseFolder.clicked.connect(self.browseFolderPath)
         self.btnAddToList.clicked.connect(self.addSelectedPathToList)
 
 
-        self.layoutPath.addWidget(self.lblPath)
-        self.layoutPath.addWidget(self.lblSelectedPath)
-        self.layoutPath.addWidget(self.btnBrowse)
-        self.layoutPath.addWidget(self.btnAddToList)
+        #layoutSelectedPath
+        self.layoutSelectedPathButton.addWidget(self.btnAddToList)
+        self.layoutSelectedPathButton.addWidget(self.btnRemoveFromList)
 
-        self.layout.addWidget(self.label2)
+        self.lblSelectedPath.setAlignment(Qt.AlignTop)
+        self.lstSelectedPath.setFixedHeight(120)
+        self.layoutSelectedPath.setAlignment(Qt.AlignTop)
 
+        self.layoutSelectedPath.addLayout(self.layoutSelectedPathButton)
+        self.layoutSelectedPath.addWidget(self.lstSelectedPath)
+
+        #layoutAnalyzer
+        self.layoutAnalyzer.addWidget(self.btnAnalyzeAll)
+        self.layoutAnalyzer.addWidget(self.btnAnalyzeSelected)
+        self.layoutAnalyzer.addWidget(self.btnClearAnalyze)
+
+        #mainLayout
         self.mainLayout.addLayout(self.layoutPath)
-        self.mainLayout.addLayout(self.layout)
+        self.mainLayout.addLayout(self.layoutSelectedPath)
+        self.mainLayout.addLayout(self.layoutAnalyzer)
 
         self.container.setLayout(self.mainLayout)
 
-    def browsePath(self):
-        pass
+        # Set the central widget of the Window.
+        self.setCentralWidget(self.container)
+
+    def browseFilePath(self):
+        dlg = QFileDialog()
+        if dlg.exec_():
+            self.edtCurrentSelectedPath.setText(dlg.selectedFiles()[0])
+
+    def browseFolderPath(self):
+        self.edtCurrentSelectedPath.setText(QFileDialog.getExistingDirectory(self, 'Hey! Select a Folder', options=QFileDialog.ShowDirsOnly))
 
     def addSelectedPathToList(self):
         pass
