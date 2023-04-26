5d3d70fce4dffb99717f3a5a9f4da23f521920a9
Heydarchi <mhheidarchi2003@yahoo.com>
2023-04-26 20:16:11 +0200

    Remove # after cleaning the line

--- app/analyzer/AnalyzerUtility.py
+++ app/analyzer/AnalyzerUtility.py
@@ -2,7 +2,10 @@ def clean_line(input_str):
     # print(input_str)
     if "#" in input_str:
         if input_str.strip().index("#") > 0:
-            cleaned_str = input_str.strip()[: input_str.index("#")].strip()
+            # cleaned_str = input_str.strip()[: input_str.index("#")].strip()
+            cleaned_str = input_str.split("#")[
+                0
+            ].strip()  # [: input_str.index("#")].strip()
             if cleaned_str == "" or cleaned_str is None:
                 return None
             return cleaned_str
